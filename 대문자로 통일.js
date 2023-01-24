6dbe7e69418d8c47b6a85b3265bb6cc3f778059c
Robinkim93 <Robinkim93@gmail.com>
2023-01-24 22:27:02 +0900

    대문자로 통일 2번째 풀이 함수명 변경

--- 대문자로 통일.js
+++ 대문자로 통일.js
@@ -2,7 +2,7 @@ function solution(str) {
   return str.toUpperCase();
 }
 
-function solution(str) {
+function solution2(str) {
   let answer = "";
   for (let x of str) {
     if (x === x.toLowerCase()) answer += x.toUpperCase();
@@ -20,9 +20,4 @@ console.log(solution(str));
  * 2. 다른 풀이로는 str의 요소를 순회하는 for of문을 선언 후,
  * 3. 요소가 toLowerCase()라면 toUpperCase()를 사용해서 대문자로 바꿔주고
  * 4. 아니라면 그냥 그대로 넣어주는 식으로 작성 할 수 있음
- *
- * for(let x of str) {
- * if(x === x.toLowerCase()) answer += x.toUpperCase()
- * else answer += x
- * }
  */
